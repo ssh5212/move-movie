3d06dd0ca2a0b7b8f80eb7413f29b9d862480cdc
AngelPlayer <ssh5212@naver.com>
2023-05-18 00:02:17 +0900

    add: header 파일 생성
    #1

--- vue-move-movie/src/main.js
+++ vue-move-movie/src/main.js
@@ -1,12 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-Vue.config.productionTip = false
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App),
+}).$mount('#app');
---
+++ vue-move-movie/src/views/HeaderView.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div id="header">
+            <b-navbar toggleable="md">
+                <b-container fluid="xl">
+                    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+                    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+                    <b-collapse id="nav-collapse" is-nav>
+                        <b-navbar-nav>
+                            <b-nav-item href="#">영화 찾아보기</b-nav-item>
+                            <b-nav-item href="#">버킷 살펴보기</b-nav-item>
+                            <b-nav-item href="#">공지사항</b-nav-item>
+                        </b-navbar-nav>
+                    </b-collapse>
+                </b-container>
+            </b-navbar>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderView',
+    components: {},
+    data() {
+        return {
+            userid: '',
+        };
+    },
+    methods: {},
+};
+</script>
+
+<style scoped>
+#header {
+    background-color: #f8edeb;
+}
+</style>
