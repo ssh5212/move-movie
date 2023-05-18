db3dd9760ec34317eea77d42d94fc3c5a6fbb802
신승헌 <ssh5212@naver.com>
2023-05-18 14:37:03 +0900

    add: FooterVue.vue 파일 생성

---
+++ vue-move-movie/src/components/layout/FooterView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="footer"></div>
+</template>
+
+<script>
+export default {
+    name: "FooterView",
+    components: {},
+    data() {
+        return {
+            message: "",
+        };
+    },
+    created() {},
+    methods: {},
+};
+</script>
+
+<style scoped></style>
