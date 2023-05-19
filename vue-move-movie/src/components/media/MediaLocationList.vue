fb25a3918647d7409c00468e7b7063cbe1aadf81
신승헌 <ssh5212@naver.com>
2023-05-19 14:49:57 +0900

    add: media 파일 추가 #8

---
+++ vue-move-movie/src/components/media/MediaLocationList.vue
@@ -0,0 +1,19 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    name: "MediaTitleList",
+    components: {},
+    data() {
+        return {
+            message: "",
+        };
+    },
+    created() {},
+    methods: {},
+};
+</script>
+
+<style scoped></style>
---
+++ vue-move-movie/src/views/MediaView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    name: "MediaView",
+    components: {},
+    data() {
+        return {
+            message: "",
+        };
+    },
+    created() {},
+    methods: {},
+};
+</script>
+
+<style scoped></style>
