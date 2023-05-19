e8806505eb85b608330f6503b272c0d72f433a61
신승헌 <ssh5212@naver.com>
2023-05-19 10:11:45 +0900

    fix: 공지사항 리스트 화면 수정 #5

--- vue-move-movie/src/components/notice/NoticeList.vue
+++ vue-move-movie/src/components/notice/NoticeList.vue
@@ -1,83 +1,84 @@
 <template>
-  <div>
-    <div class="row mb-3">
-      <b-col class="col-md-1"></b-col>
-      <h1>공지사항</h1>
-      <b-col class="col-md-11"></b-col>
-      <!-- <b-button class="col-md-1" variant="success" @click="moveWrite">
-        등록
-      </b-button> -->
-      <button
-        type="button"
-        class="col-md-1 btn btn-secondary"
-        @click="moveWrite"
-      >
-        등록
-      </button>
+    <div>
+        <!-- [S] Intro Image -->
+        <div class="jb-box">
+            <div class="top-img">
+                <img src="@/assets/img/intro-half-img01.jpg" alt="" width="1920" height="auto" />
+            </div>
+
+            <div class="jc-text">
+                <p class="vtext-big">공지사항</p>
+            </div>
+        </div>
+        <!-- [E] Intro Image -->
+
+        <!-- [S] body -->
+        <div class="container">
+            <div class="row justify-content-xl-end my-5 align-items-center">
+                <div class="col-md-auto">
+                    <button type="button" class="btn btn-secondary my-3" @click="moveWrite">등록</button>
+                </div>
+                <div class="col-md-12">
+                    <b-table striped hover :items="noticeList" :fields="fields" @row-clicked="movedetail"></b-table>
+                </div>
+            </div>
+        </div>
+        <!-- [E] body -->
     </div>
-    <b-table
-      striped
-      hover
-      :items="noticeList"
-      :fields="fields"
-      @row-clicked="movedetail"
-    >
-    </b-table>
-  </div>
 </template>
 
 <script>
 export default {
-  name: "NoticeList",
-  components: {},
-  data() {
-    return {
-      fields: [],
-      noticeList: [
-        {
-          notice_pk: 0,
-          notice_title: "Dickerson",
-          notice_content: "Macdonald",
-          hit: 17,
-          user_pk: 13,
-        },
-        {
-          notice_pk: 1,
-          notice_title: "ersonDick",
-          notice_content: "Macdondon",
-          hit: 17,
-          user_pk: 13,
-        },
-        {
-          notice_pk: 2,
-          notice_title: "Dirsoncke",
-          notice_content: "Mdonanald",
-          hit: 17,
-          user_pk: 13,
-        },
-        {
-          notice_pk: 3,
-          notice_title: "kersonDickerson",
-          notice_content: "Macdonald",
-          hit: 17,
-          user_pk: 13,
-        },
-      ],
-    };
-  },
-  created() {},
-  methods: {
-    movedetail(item) {
-      this.$router.push({
-        name: `noticeDetail`,
-        params: { no: item.notice_pk },
-      });
+    name: "NoticeList",
+    components: {},
+    data() {
+        return {
+            fields: [],
+            noticeList: [
+                {
+                    notice_pk: 0,
+                    notice_title: "Dickerson",
+                    notice_content: "Macdonald",
+                    hit: 17,
+                    user_pk: 13,
+                },
+                {
+                    notice_pk: 1,
+                    notice_title: "ersonDick",
+                    notice_content: "Macdondon",
+                    hit: 17,
+                    user_pk: 13,
+                },
+                {
+                    notice_pk: 2,
+                    notice_title: "Dirsoncke",
+                    notice_content: "Mdonanald",
+                    hit: 17,
+                    user_pk: 13,
+                },
+                {
+                    notice_pk: 3,
+                    notice_title: "kersonDickerson",
+                    notice_content: "Macdonald",
+                    hit: 17,
+                    user_pk: 13,
+                },
+            ],
+        };
     },
+    created() {},
+    methods: {
+        movedetail(item) {
+            this.$router.push({
+                name: `noticeDetail`,
+                params: { no: item.notice_pk },
+            });
+        },
 
-    moveWrite() {
-      this.$router.push({ name: `noticeWrtie` });
+        moveWrite() {
+            this.$router.push({ name: `noticeWrtie` });
+        },
     },
-  },
 };
 </script>
 
--- vue-move-movie/src/views/NoticeView.vue
+++ vue-move-movie/src/views/NoticeView.vue
@@ -1,30 +1,18 @@
 <template>
-  <div>
-    <!-- [S] body -->
-    <div class="container">
-      <!-- 실제 내용 -->
-      <div class="row justify-content-xl-center my-5 align-items-center">
-        <div class="col-md-6 offset-md-3 mt-3">
-          <h2>Notice</h2>
-        </div>
-      </div>
-      <router-view />
-    </div>
-    <!-- [E] body -->
-  </div>
+    <router-view />
 </template>
 
 <script>
 export default {
-  name: "NoticeView",
-  components: {},
-  data() {
-    return {
-      message: "",
-    };
-  },
-  created() {},
-  methods: {},
+    name: "NoticeView",
+    components: {},
+    data() {
+        return {
+            message: "",
+        };
+    },
+    created() {},
+    methods: {},
 };
 </script>
 
