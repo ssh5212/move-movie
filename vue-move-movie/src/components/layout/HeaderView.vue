710db02de62d354b160cee657bb937ba88525133
AngelPlayer <ssh5212@naver.com>
2023-05-20 13:51:15 +0900

    feat: 헤더 부분 사이드바 연결 기능 추가  #4

--- vue-move-movie/src/components/layout/HeaderView.vue
+++ vue-move-movie/src/components/layout/HeaderView.vue
@@ -1,95 +1,85 @@
 <template>
-  <div>
-    <div id="header">
-      <b-navbar toggleable="md" class="fixed-top py-3" :style="headerColor">
-        <b-container fluid="xl">
-          <!-- 로고 -->
-          <b-navbar-brand href="#">
-            <router-link to="/">
-              <img
-                src="@/assets/logo.png"
-                class="d-inline-block align-middle header-logo"
-                width="100px"
-                alt="ssafy"
-              />
-            </router-link>
-            <span>MoveMovie</span>
-          </b-navbar-brand>
-          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-          <b-collapse id="nav-collapse" is-nav>
-            <b-navbar-nav>
-              <b-nav-item href="#">영화 찾아보기</b-nav-item>
-              <b-nav-item href="/bucket">버킷 살펴보기</b-nav-item>
-              <b-nav-item href="/notice">공지사항</b-nav-item>
-            </b-navbar-nav>
-          </b-collapse>
-          <!-- 오른쪽 사이드바 버튼 -->
-          <b-collapse
-            id="nav-collapse"
-            class="justify-content-end d-none d-md-block"
-            is-nav
-          >
-            <b-icon-person-square
-              id="b-icon-person-square"
-              class="justify-content-end h2 pt-1"
-              v-b-toggle.sidebar-backdrop
-            ></b-icon-person-square>
-          </b-collapse>
-        </b-container>
-      </b-navbar>
+    <div>
+        <div id="header">
+            <b-navbar toggleable="md" class="fixed-top py-3" :style="headerColor">
+                <b-container fluid="xl">
+                    <!-- 로고 -->
+                    <b-navbar-brand href="#">
+                        <router-link to="/">
+                            <img src="@/assets/logo.png" class="d-inline-block align-middle header-logo mr-2" width="100px" alt="ssafy" />
+                        </router-link>
+                        <span>MoveMovie</span>
+                    </b-navbar-brand>
+                    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+                    <b-collapse id="nav-collapse" is-nav>
+                        <b-navbar-nav>
+                            <b-nav-item href="#">영화 찾아보기</b-nav-item>
+                            <b-nav-item href="/bucket">버킷 살펴보기</b-nav-item>
+                            <b-nav-item href="/notice">공지사항</b-nav-item>
+                        </b-navbar-nav>
+                    </b-collapse>
+                    <!-- 오른쪽 사이드바 버튼 -->
+                    <b-collapse id="nav-collapse" class="justify-content-end d-none d-md-block" is-nav>
+                        <b-icon-basket2-fill id="b-icon-basket2-fill" class="justify-content-end h2 pt-1 mr-3" v-b-toggle.sidebar-left></b-icon-basket2-fill>
+                        <b-icon-person-square id="b-icon-person-square" class="justify-content-end h2 pt-1" v-b-toggle.sidebar-backdrop></b-icon-person-square>
+                    </b-collapse>
+                </b-container>
+            </b-navbar>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import { throttle } from "lodash";
+import { throttle } from 'lodash';
 export default {
-  name: "HeaderView",
-  components: {},
-  data() {
-    return {
-      target: null, // 스크롤 이벤트 저장 변수
-      headerColor: "background-color: white; opacity:0.9;", // 스크롤 변경 시 마다 색상 변경 바인딩
-      scrollTop: 0, // 스크롤 위치 저장 변수
-    };
-  },
-
-  mounted() {
-    // 화면 전환시 이벤트 생성
-    this.target = throttle(this.handleScroll, 1000); // 쓰로틀링 적용 (1초)
-    window.addEventListener("scroll", this.handleScroll);
-  },
-
-  beforeUnmount() {
-    // 화면 전환시 기존 이벤트 제거
-    window.removeEventListener("scroll", this.handleScroll);
-  },
-
-  methods: {
-    // 현재 스크롤 위치를 확인하고 색상 변경 여부 판단
-    handleScroll() {
-      if (window.pageYOffset > 0) {
-        this.headerColor = "background-color: white; opacity:0.9;";
-      } else {
-        this.headerColor = "background-color: white; opacity:0.9;";
-      }
+    name: 'HeaderView',
+    components: {},
+    data() {
+        return {
+            target: null, // 스크롤 이벤트 저장 변수
+            headerColor: 'background-color: white; opacity:0.9;', // 스크롤 변경 시 마다 색상 변경 바인딩
+            scrollTop: 0, // 스크롤 위치 저장 변수
+        };
     },
-  },
+
+    mounted() {
+        // 화면 전환시 이벤트 생성
+        this.target = throttle(this.handleScroll, 1000); // 쓰로틀링 적용 (1초)
+        window.addEventListener('scroll', this.handleScroll);
+    },
+
+    beforeUnmount() {
+        // 화면 전환시 기존 이벤트 제거
+        window.removeEventListener('scroll', this.handleScroll);
+    },
+
+    methods: {
+        // 현재 스크롤 위치를 확인하고 색상 변경 여부 판단
+        handleScroll() {
+            if (window.pageYOffset > 0) {
+                this.headerColor = 'background-color: white; opacity:0.9;';
+            } else {
+                this.headerColor = 'background-color: white; opacity:0.9;';
+            }
+        },
+    },
 };
 </script>
 
 <style scoped>
 .header-logo {
-  height: 35px;
-  width: auto;
+    height: 35px;
+    width: auto;
 }
 
 /* 버튼처럼 호버되면 변하게 변경 */
-#b-icon-person-square {
-  color: rgba(0, 0, 0, 0.5);
+#b-icon-person-square,
+#b-icon-basket2-fill {
+    color: rgba(0, 0, 0, 0.5);
 }
-#b-icon-person-square:hover {
-  background-color: rgba(0, 0, 0, 0.7);
-  cursor: pointer;
+#b-icon-person-square:hover,
+#b-icon-basket2-fill:hover {
+    color: rgba(0, 0, 0, 0.7);
+    cursor: pointer;
 }
 </style>
