c836b6b6abe7b10893152b1e857bb8f4e37797b8
AngelPlayer <ssh5212@naver.com>
2023-05-20 19:04:13 +0900

    add: media 출력용 화면 생성 #8

--- vue-move-movie/src/components/media/MediaTitleList.vue
+++ vue-move-movie/src/components/media/MediaTitleList.vue
@@ -15,20 +15,7 @@
         <!-- [S] body -->
         <div class="container">
             <div class="row justify-content-xl-center my-5 align-items-center">
-                <!-- 영화 1 -->
-                <div class="col-md-3 my-4 position-relative">
-                    <div class="card bg-dark text-white img-bg-dark">
-                        <div class="img-bg-dark">
-                            <!-- 이게 먼저 클릭되니 여기에 옵션 주고 onclick 주면 될듯 -->
-                            <img src="https://img.cgv.co.kr/Movie/Thumbnail/Poster/000087/87032/87032_320.jpg" class="card-img" name="1" @click="moveSpotlist" />
-                        </div>
-                    </div>
-                    <div class="card-overlay">
-                        <h3 class="h3 card-text">Card title</h3>
-                        <p class="card-text mb-1">사랑시 고백구 행복동 11번가 37번리 맛집입니다.</p>
-                        <p class="card-text mb-1">Last updated 3 mins ago</p>
-                    </div>
-                </div>
+                <!-- 여기에 Media Item 생성 -->
             </div>
         </div>
         <!-- [E] body -->
---
+++ vue-move-movie/src/components/media/MediaTitleListItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <!-- 영화 1 -->
+        <div class="col-md-3 my-4 position-relative">
+            <div class="card bg-dark text-white img-bg-dark">
+                <div class="img-bg-dark">
+                    <!-- 이게 먼저 클릭되니 여기에 옵션 주고 onclick 주면 될듯 -->
+                    <img src="https://img.cgv.co.kr/Movie/Thumbnail/Poster/000087/87032/87032_320.jpg" class="card-img" name="1" @click="moveSpotlist" />
+                </div>
+            </div>
+            <div class="card-overlay">
+                <h3 class="h3 card-text">Card title</h3>
+                <p class="card-text mb-1">사랑시 고백구 행복동 11번가 37번리 맛집입니다.</p>
+                <p class="card-text mb-1">Last updated 3 mins ago</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
