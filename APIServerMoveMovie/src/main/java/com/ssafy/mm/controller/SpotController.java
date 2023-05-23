08ba10fec40d29acdc67ab579711cba780c6685c
JJewon <eldertree777@gmail.com>
2023-05-23 10:26:37 +0900

    test

--- APIServerMoveMovie/src/main/java/com/ssafy/mm/controller/SpotController.java
+++ APIServerMoveMovie/src/main/java/com/ssafy/mm/controller/SpotController.java
@@ -1,6 +1,5 @@
 package com.ssafy.mm.controller;
 
-import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,11 +12,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.mm.model.SidoGugunCodeDto;
